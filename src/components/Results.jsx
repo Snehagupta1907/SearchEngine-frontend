b6176586deed277fdfaf80947a2e1d7b706e2ea2
Snehagupta1907 <snehagupta98930@gmail.com>
2023-07-10 23:30:44 +0530

    changes

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,13 +4,30 @@ import ReactPlayer from 'react-player';
 import { useResultContext } from '../contexts/ResultContextProvider';
 import Loading from './Loading';
 
-
-
 const Results = () => {
     const { results, loading, getResults, searchTerm }=useResultContext();
     const location=useLocation();
 
     if(loading) return <Loading/>
+
+    switch (location.pathname) {
+        case '/search':
+            return 'SEARCH';
+            break;
+        case '/images':
+            return 'image';
+            break;
+        case '/news':
+            return 'news';
+            break;
+
+        case '/videos':
+            return 'videos';
+            break;
+        default:
+            return 'ERROR';
+            break;
+    }
   return (
     <div>Results</div>
   )
